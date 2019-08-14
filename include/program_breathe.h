4322eb787b3293649b93686a1ee35be857ab20cb
Harald Dahl <harald@dira.no>
2019-08-14 20:55:33 +0200

    Created files for the new program

---
+++ include/program_breathe.h
@@ -0,0 +1,9 @@
+#ifndef __BREATHE_H_INCLUDED_
+#define __BREATHE_H_INCLUDED_
+
+#include "main.h"
+
+void breathe();
+void breatheUpdate();
+
+#endif
---
+++ src/program_breathe.cpp
@@ -0,0 +1,18 @@
+breathe()
+{
+    active_program = BREATHE;
+    total_steps1 = 255;
+    interval = 10;
+    hue1 = 200;
+    saturation1 = 200;
+    update = true;
+}
+
+breatheUpdate()
+{
+    float breath = exp(sin8(millis()))
+    for (uint8_t x; x < NUM_LEDS_PER_STRIP; x++)
+    {
+
+    }
+}
