95e85d9968eb4f1419e343027f229519f53c673b
Harald Dahl <harald@dira.no>
2019-08-30 22:20:44 +0200

    All stages of the firework is functioning

--- include/program_fireworks.h
+++ include/program_fireworks.h
@@ -8,6 +8,8 @@
 #include "main.h"
 
 #define MAX_NUMBER_OF_SPARKS 60       // The maximum number of sparks from a firework explosion
+#define MIN_NUMBER_OF_SPARKS 20       // The minimum number of sparks from a firework explosion
+#define SPARK_FADE_TIME 5000          // Aount of miliseconds to stay in FADE stage
 
 extern CRGBPalette16 explosion_palette;
 extern CRGBPalette16 spark_palette;
@@ -25,34 +27,36 @@ class Firework
             FADING,
             WAITING
         };
-        stage current_stage;                                // What stage of the firework are we currently at
-        int next_launch_time;                               // Time for next flare launch
-        uint16_t next_launch_max_wait;                      // Maximum time to wait for next launch
-        uint16_t next_launch_min_wait;                      // Minimum time to wait for next launch
-        bool launched;                                      // Is the flare launched yet?
-        float flare_velocity;                               // Speed of the flare when it is launched
-        uint16_t flare_position;                            // Current position of the flare
-        uint8_t strip_number;                               // What strip this firework object is displayed on
-        float gravity;                                      // Gravity makes things accelerate back down
-        uint8_t number_of_sparks;                           // Number of sparks from explosion
-        uint16_t spark_position[MAX_NUMBER_OF_SPARKS];      // Position of all the sparks
-        float spark_velocity[MAX_NUMBER_OF_SPARKS];         // The current velocity of all the sparks
-        uint8_t spark_gradient_index[MAX_NUMBER_OF_SPARKS]; // The index of the active_spark_palette this spark gets its color from
-        CRGBPalette16 *active_spark_palette;                // The palette this firework object will get colors from
-        bool get_random_palette;                            // True = random palette will be picked when new flare is generated. False = will get palette from the global active_palette 
-        uint8_t flare_hue;                                  // Hue of the flare
-        uint8_t flare_sat;                                  // Saturation of the flare
-        uint8_t flare_val;                                  // Brightness of the flare
-        uint8_t flare_tail_length;                          // How much to fade the tail of the flare each iteration (determines length of the "tail")
+        stage current_stage;                                    // What stage of the firework are we currently at
+        int next_launch_time;                                   // Time for next flare launch
+        uint16_t next_launch_max_wait;                          // Maximum time to wait for next launch
+        uint16_t next_launch_min_wait;                          // Minimum time to wait for next launch
+        bool launched;                                          // Is the flare launched yet?
+        float flare_velocity;                                   // Speed of the flare when it is launched
+        float flare_position;                                   // Current position of the flare
+        uint8_t strip_number;                                   // What strip this firework object is displayed on
+        float gravity;                                          // Gravity makes things accelerate back down
+        uint8_t number_of_sparks;                               // Number of sparks from explosion
+        float spark_position[MAX_NUMBER_OF_SPARKS];          // Position of all the sparks
+        float spark_velocity[MAX_NUMBER_OF_SPARKS];             // The current velocity of all the sparks
+        uint8_t spark_gradient_index[MAX_NUMBER_OF_SPARKS];     // The index of the active_spark_palette this spark gets its color from
+        bool spark_velocity_direction[MAX_NUMBER_OF_SPARKS];    // The direction of the spark
+        CRGBPalette16 *active_spark_palette;                    // The palette this firework object will get colors from
+        bool get_random_palette;                                // True = random palette will be picked when new flare is generated. False = will get palette from the global active_palette 
+        uint8_t flare_hue;                                      // Hue of the flare
+        uint8_t flare_sat;                                      // Saturation of the flare
+        uint8_t flare_val;                                      // Brightness of the flare
+        uint8_t flare_tail_length;                              // How much to fade the tail of the flare each iteration (determines length of the "tail")
+        uint32_t spark_fade_end;                                     // Time to end FADE stage and proceed to next flare launch
         
     public:
-        Firework(uint8_t strip_n);                          // The firework class constructor
-        void prepare_for_launch();                          // This function resets all positions, and generates new random values
-        void launch();                                      // This function handles updating of velocities, positions, colors, etc for the flare in the "launch part" of the animation
-        void explosion();                                   // This function handles updating of velocities, positions, colors, etc for the sparks in the "explosion part" of the animation
-        void fading();                                      // This function handles updating of velocities, positions, colors, etc for the sparks in the "fading part" of the animation
-        void update_firework();                             // This function check the stage of the firework, and calls the appropriate function
-        void set_palette(CRGBPalette16 *palette);           // Function for updating the active_spark_palette variable of the Firework object 
+        Firework(uint8_t strip_n);                              // The firework class constructor
+        void prepare_for_launch();                              // This function resets all positions, and generates new random values
+        void launch();                                          // This function handles updating of velocities, positions, colors, etc for the flare in the "launch part" of the animation
+        void explosion();                                       // This function handles updating of velocities, positions, colors, etc for the sparks in the "explosion part" of the animation
+        void fading();                                          // This function handles updating of velocities, positions, colors, etc for the sparks in the "fading part" of the animation
+        void update_firework();                                 // This function check the stage of the firework, and calls the appropriate function
+        void set_palette(CRGBPalette16 *palette);               // Function for updating the active_spark_palette variable of the Firework object 
 };
 
 extern Firework firework0;
--- src/program_fireworks.cpp
+++ src/program_fireworks.cpp
@@ -6,9 +6,9 @@ DEFINE_GRADIENT_PALETTE(explosion_gp){
 128, 255, 0, 0};     //red
 
 DEFINE_GRADIENT_PALETTE(sparks_gp){
-255, 255, 255, 255, //full white
-224, 255, 255, 0,   //bright yellow
-128, 255, 0, 0};    //red
+0, 255, 0, 0,      //red
+224, 255, 255, 0,    //bright yellow
+255, 255, 255, 255}; //full white
 
 CRGBPalette16 explosion_palette = explosion_gp;
 CRGBPalette16 spark_palette = sparks_gp;
@@ -34,7 +34,7 @@ Firework *firework_array[NUM_STRIPS*STRIP_SPLIT] = {&firework0, &firework1, &fir
 
 void fireworks()
 {
-    interval = 1;
+    interval = 30;
     active_program = FIREWORKS;
     FastLED.clear();
     FastLED.show();
@@ -47,7 +47,6 @@ void fireworks()
 
 void fireworksUpdate()
 {
-    Serial.println("Hit da?");
     for (uint8_t i = 0; i < NUM_STRIPS*STRIP_SPLIT; i++)
     {
         firework_array[i]->update_firework();
@@ -60,83 +59,126 @@ Firework::Firework(uint8_t strip_n)
 {
     strip_number = strip_n;
     current_stage = WAITING;                           
-    gravity = 0.95;
+    gravity = 0.99;
     active_spark_palette = &spark_palette;
     get_random_palette = true;
     next_launch_max_wait = 30000;
-    next_launch_min_wait = 1000;
+    next_launch_min_wait = 10000;
     flare_hue = 10;
     flare_sat = 255;
     flare_val = 255;
-    flare_tail_length = 10;
+    flare_tail_length = 100;
 }
 
 void Firework::prepare_for_launch()
 {
-    Serial.println("Preparing for launch!");
     random16_add_entropy(random());
     current_stage = LAUNCH;
     flare_position = 0.0;
-    next_launch_time = millis() + random16(next_launch_min_wait,next_launch_max_wait);
-    flare_velocity = float(random16(50,90))/100;
+    int random_add = random16(next_launch_min_wait, next_launch_max_wait);
+    next_launch_time = millis() + random_add;
+    flare_velocity = float(random16(50,100))/100;
     launched = false;
     // Reset all LEDs of the strip
-    for (uint16_t i; i < NUM_LEDS_PER_STRIP/STRIP_SPLIT; i++)
+    for (uint16_t i = 0; i < NUM_LEDS_PER_STRIP/STRIP_SPLIT; i++)
     {
         leds[led_order_array[(strip_number * (NUM_LEDS_PER_STRIP / STRIP_SPLIT)) + i]] = CRGB::Black;
     }
-
 }
 
 void Firework::launch()
 {
-    Serial.println("Her");
-    if (launched & flare_velocity < 0.1) {
-        Serial.println("Time to explode");
+    // If the flare has slowed down to this value, we will go to the next stage: EXPLOSION!
+    if (launched & (flare_velocity < 0.1))
+    {
+        // Prepare spark variables for explosion
         current_stage = EXPLOSION;
+        number_of_sparks = random8(MIN_NUMBER_OF_SPARKS, MAX_NUMBER_OF_SPARKS);
+        for (uint8_t x = 0; x < number_of_sparks; x++)
+        {
+            spark_gradient_index[x] = random8();
+            spark_position[x] = flare_position;
+            spark_position[x] = flare_position;
+            spark_velocity[x] = float(random16(10, 125)) / 100;
+            spark_velocity_direction[x] = random8(0,2);
+        }
     }
     else
     {   
         if (!launched)
         {
-            Serial.println("We are launching!");
             leds[led_order_array[(strip_number*(NUM_LEDS_PER_STRIP/STRIP_SPLIT))+(int)flare_position]] = CHSV(flare_hue,flare_sat,flare_val);
             launched = true;
         }
         else
         {
-            Serial.println("We are in the air and rising");
-            // Fading the tail/trail of the flare
+            // Fading the tail/trail of the flare. Only applying fadeToBlackBy() to the pixels with an index lower than (int)flare_position.
+            // NB: Casting flare_position (which is a float) to int, will floor the value
             for (uint16_t i = 0; i < (int)flare_position; i++)
             {
                 leds[led_order_array[(strip_number * (NUM_LEDS_PER_STRIP / STRIP_SPLIT)) + i]].fadeToBlackBy(flare_tail_length);
             }
-            // Dunno if this works! o.O
-            // The idea is to get the fraction from flare position (pixel_spill), and use this fraction to dim "current position" and start lighting up the next LED
+            // The idea is to get the fraction from flare position (pixel_spill), and use this fraction to start lighting up the next LED
             // The result should then be that the pixel glides along the strip instead of jumping from pixel to pixel
-            float pixel_spill = flare_position - (int)flare_position; 
-            leds[led_order_array[(strip_number * (NUM_LEDS_PER_STRIP / STRIP_SPLIT)) + (int)flare_position]] = CHSV(flare_hue, flare_sat, (flare_val*pixel_spill));
-            leds[led_order_array[(strip_number * (NUM_LEDS_PER_STRIP / STRIP_SPLIT)) + (int)flare_position]+1] = CHSV(flare_hue, flare_sat, (flare_val * pixel_spill));
+            float pixel_spill = ((int)flare_position + 1) - flare_position;
+            leds[led_order_array[(strip_number * (NUM_LEDS_PER_STRIP / STRIP_SPLIT)) + (int)flare_position]+1] = CHSV(flare_hue, flare_sat, (flare_val * (1 - pixel_spill)));
         }
         flare_position += flare_velocity;       // New position = current + velocity
-        Serial.print("Flare position: ");
-        Serial.println(flare_position);
         flare_velocity *= gravity; // New velocity = current * gravity (gravity is between 0 and 1, so velocity will decrease)
-        Serial.print("Flare velocity: ");
-        Serial.println(flare_velocity);
     }
 }
 
 void Firework::explosion()
 {
-    Serial.println("EXPLOSION!!!");
-    // Code here
-    current_stage = WAITING;
+    for (uint16_t x = 0; x < NUM_LEDS_PER_STRIP / STRIP_SPLIT; x++)
+    {
+        leds[led_order_array[(strip_number * (NUM_LEDS_PER_STRIP / STRIP_SPLIT)) + x]].fadeToBlackBy(128);
+    }
+
+    for (uint8_t x = 0; x < number_of_sparks; x++)
+    {
+        // Set pixel values
+        uint16_t pixel = led_order_array[(strip_number * (NUM_LEDS_PER_STRIP / STRIP_SPLIT)) + (int)spark_position[x]];
+        CRGB pixel_color = ColorFromPalette(*active_spark_palette, spark_gradient_index[x]);
+        leds[pixel] += pixel_color;
+
+        // Update spark parameters
+        if (spark_velocity_direction[x]) {
+            spark_position[x] += spark_velocity[x];
+        }
+        else {
+            spark_position[x] -= spark_velocity[x];
+        }
+        // Constrain position to be within the current LED strip
+        spark_position[x] = constrain(spark_position[x],0.0,(float)(NUM_LEDS_PER_STRIP/STRIP_SPLIT));
+        spark_velocity[x] *= gravity;
+        
+    }
+
+    // Check how fast the fastest spark is. When the velocity of the fastest spark passes a threshold we move to the next stage
+    float fastest_spark = 0.0;
+    for (uint8_t x = 0; x < number_of_sparks; x++)
+    {
+        if (spark_velocity[x] > fastest_spark) {
+            fastest_spark = spark_velocity[x];
+        }
+    }
+    if (fastest_spark < 0.3) {
+        current_stage = FADING;
+        spark_fade_end = millis() + SPARK_FADE_TIME;
+    }
 }
 
 void Firework::fading()
 {
-    // Code here
+    for (uint16_t x = 0; x < NUM_LEDS_PER_STRIP / STRIP_SPLIT; x++)
+    {
+        leds[led_order_array[(strip_number * (NUM_LEDS_PER_STRIP / STRIP_SPLIT)) + x]].fadeToBlackBy(8);
+    }
+
+    if (millis() > spark_fade_end) {
+        current_stage = WAITING;
+    }
 }
 
 void Firework::set_palette(CRGBPalette16 *palette)
@@ -147,7 +189,6 @@ void Firework::set_palette(CRGBPalette16 *palette)
 void Firework::update_firework()
 {
     if (current_stage == WAITING) {
-        Serial.println("Kommer hit");
         prepare_for_launch();
     }
     else if (current_stage == LAUNCH) {
